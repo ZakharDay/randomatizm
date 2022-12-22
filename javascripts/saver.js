4e409c9a3c8348f12d685b41a6c722d46d2499f9
ZakharDay <zakhar.day@gmail.com>
2022-12-22 06:02:36 +0300

    Sample function added

--- javascripts/saver.js
+++ javascripts/saver.js
@@ -1,3 +1,7 @@
+function sample(array) {
+  return array[Math.floor(Math.random() * array.length)]
+}
+
 function generateHash() {
   const symbols = ['a', 'b', 'c', 'd', 'e', 'f', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
   let hash = ''
